86328d47385d51050a1f8048dd98591385500bbe
Marcox-try083 <vishalnitjsr2605ee@gmail.com>
2026-07-17 15:08:16 +0530

    fix:

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,8 @@ export default function App() {
 
   useEffect(() => {
   axios
-    .get("http://localhost:5000/api/profile-stats")
+    axios
+  .get(`${import.meta.env.VITE_API_URL}/api/profile-stats`)
     .then((res) => setStats(res.data))
     .catch((err) => console.error(err));
 }, []);
